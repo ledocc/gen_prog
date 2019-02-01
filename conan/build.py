1bad8cca57b0e982ba657680c58fa197bc9bb930
David Callu <callu.david@gmail.com>
2019-02-01 19:12:39 +0100

    chore(ci): use libstdc++11 on linux

--- conan/build.py
+++ conan/build.py
@@ -3,5 +3,16 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="gen_prog:shared")
+    builder.add_common_builds()
+
+    if platform.system() == "Linux":
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            if ( settings["compiler"] == "clang" ) or ( settings["compiler"] == "gcc" ):
+                settings_libstdcxx11 = settings.copy()
+                settings_libstdcxx11["compiler.libcxx"] = "libstdc++11"
+                filtered_builds.append([settings_libstdcxx11, options, env_vars, build_requires])
+            filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
+
     builder.run()
